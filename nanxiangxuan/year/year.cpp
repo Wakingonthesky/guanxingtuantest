bf5b4032ca06b830dcbeb9c43cba4f58ea2cfc9c
nxx1213 <98649362+nxx1213@users.noreply.github.com>
2022-07-30 20:54:40 +0800

    Add files via upload

---
+++ nanxiangxuan/year/year.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("the year you want\n");
+    scanf_s("%d",&a);
+    if (a % 100 == 0)
+        if (a % 400 == 0)
+            printf("这是闰年");
+        else printf("这不是闰年");
+    if (a%100 !=0 &&a % 4 == 0)
+        printf("这是闰年");
+    else printf("这不是闰年");
+}
+
